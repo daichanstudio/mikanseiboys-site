a220f0511a25244d6ac99588421a3d7e2bc21540
daichi <w.daichi2835@gmail.com>
2024-03-31 15:06:44 +0900

    fix bugs

--- src/app/(about)/contact/page.js
+++ src/app/(about)/contact/page.js
@@ -11,7 +11,7 @@ export default function Contact(){
 
   return <section className="w-full h-auto md:h-[75vh] border-b-2 border-solid border-dark dark:border-light flex  flex-col md:flex-row items-center justify-center text-dark dark:text-light">
 
-  {/* <div className="flex flex-col items-start justify-center px-16 pt-8 pb-8"> */}
+
   <div className="flex flex-col items-start justify-center w-full px-5 pb-8 md:w-3/5 xs:px-10 md:px-16">
   <h2 className="mt-4 text-2xl font-bold capitalize xs:text-3xl sm:text-4xl">Let's Connect!</h2>
   <ContactForm/>
